d0c0359a41cefe01515064fb2696d5ef2a0d1044
wferbt <cuperpuper820@gmail.com>
2026-01-24 23:08:31 +0500

    Absolute final build refresh

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -4,8 +4,11 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   
-  // Добавь эту строку обязательно!
-  app.enableCors(); 
+  app.enableCors({
+  origin: '*', // Разрешить всем
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  credentials: true,
+});
   
   const port = process.env.PORT || 3000;
   await app.listen(port, '0.0.0.0');
